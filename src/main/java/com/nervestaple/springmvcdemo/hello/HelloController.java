f9e0675f04eed7894a1286c9d8ae7d61a1683039
Christopher Miles <twitch@nervestaple.com>
2022-04-27 15:03:07 -0400

    :recycle: Moved files around to keep things tidy

---
+++ src/main/java/com/nervestaple/springmvcdemo/hello/HelloController.java
@@ -0,0 +1,18 @@
+package com.nervestaple.springmvcdemo.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping(value = "/")
+    public String getString(
+            @RequestParam(name = "name", required = false, defaultValue = "World")
+            String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello/hello";
+    }
+}
